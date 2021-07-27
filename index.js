c25f62913c3fadab760883be5093fc398fcee32e
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-07-27 15:09:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ client.on('message', (message) => antiSpam.message(message));
 client.on("ready", async () => {
   console.log(`${client.user.username} is online on ${client.guilds.cache.size} servers!`);
 
-  client.user.setActivity('over unverified bots!! | r!help', {type: "WATCHING"});
+  client.user.setActivity('over server security | r!help', {type: "WATCHING"});
 });
 
 //Command Manager
