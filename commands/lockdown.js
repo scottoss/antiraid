35580cef9e233781a9139f4c39dfc0e9ca545dde
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-07-29 23:12:03 +0200

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -1,7 +1,12 @@
 const Discord = require('discord.js')
+const premium = require('./../premium.json')
+
+
 
 
 module.exports.run = async (bot, message, args) => {
+               
+      if (premium.guilds.includes(message.guild.id)) {
         let lockPermErr = new Discord.MessageEmbed()
         .setTitle("**User Permission Error!**")
         .setDescription("**Sorry, you don't have permissions to use this! ❌**")
@@ -37,9 +42,53 @@ module.exports.run = async (bot, message, args) => {
             return message.channel.send(lockEmbed2)
         }
 
-}
+      } else if (premium.users.includes(message.author.id)) {
+        let lockPermErr = new Discord.MessageEmbed()
+        .setTitle("**User Permission Error!**")
+        .setDescription("**Sorry, you don't have permissions to use this! ❌**")
+        
+        if(!message.channel.permissionsFor(message.member).has("BAN_MEMBERS") ) return message.channel.send(lockPermErr);
+
+        const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+        if (args[0] === 'on') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: false
+                })
+            })
+            
+            let lockEmbed = new Discord.MessageEmbed()
+                
+                .setThumbnail(`https://media.giphy.com/media/JozO6wdFcC81VPO6RS/giphy.gif`)
+                .setDescription(`**\n\nDone! Server Fully Locked! 🔒**`)
+                .setColor('#2F3136')
+            return message.channel.send(lockEmbed);
+
+        } else if (args[0] === 'off') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: true
+                })
+            })
+            
+            let lockEmbed2 = new Discord.MessageEmbed()
+                .setColor('#2F3136')    
+                .setThumbnail(`https://media.giphy.com/media/JozO6wdFcC81VPO6RS/giphy.gif`)
+                .setDescription(`**\n\nDone! Server Fully Unlocked! 🔓**`)
+            return message.channel.send(lockEmbed2)
+        }
+
+      } else {
+        message.channel.send('u need premium to run this command!!!');
+      }
+    }
   
   
+
 module.exports.help = {
   name:"lockdown"
 }
+
+
+  
+  
