739a94cd8cf77ad53b392f31960eb5e071330917
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-07-29 22:57:57 +0200

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,1 +1,9 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+               message.channel.send('sorry reporting isnt enabled yet ¯\_(ツ)_/¯ ');
+}
+
+module.exports.help = {
+  name:"report"
+}
