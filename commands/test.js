68dc5f6a47306782d6cc268d24e47661c591b93e
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-07-29 17:21:57 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,4 +1,8 @@
 const Discord = require("discord.js");
+const premium = require('./../premium.json')
+
+
+
 
 module.exports.run = async (bot, message, args) => {
                message.channel.send('pong');
