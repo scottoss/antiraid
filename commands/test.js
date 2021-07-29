0253001becbbd9894a0041f6b8157da3921a06ab
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-07-29 18:17:22 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -7,7 +7,7 @@ const premium = require('./../premium.json')
 
 
 module.exports.run = async (bot, message, args) => {
-               var server = client.guilds.cache.get(message.guild.id).id;
+               var server = client.guilds.get(message.guild.id).id;
                if (premium.guilds.includes(server)) {
                  message.channel.send('pong');
                } else if (premium.users.includes(message.user.cache.id)) {
