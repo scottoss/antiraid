1c788825163d8d63bccbe5c142f37ea08dd10433
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-07-29 20:07:28 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,8 +5,9 @@ const premium = require('./../premium.json')
 
 
 module.exports.run = async (bot, message, args) => {
-               
-               if (premium.users.includes(message.author.id)) {
+               if (premium.guilds.includes(message.guild.id)) {
+                 message.channel.send('pong');
+               } else if (premium.users.includes(message.author.id)) {
                  message.channel.send('pong');
                } else {
                  message.channel.send('u need premium');
