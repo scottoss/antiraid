693cad284ca594f91fdafd3f7f9b6046c8f1fa09
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-08-02 17:12:01 +0200

    Update backup.js

--- commands/backup.js
+++ commands/backup.js
@@ -6,14 +6,38 @@ const premium = require('./../premium.json')
 
 module.exports.run = async (bot, message, args) => {
                
-               if (premium.guilds.includes(message.guild.id)) {
-                 message.channel.send('guild has premium'); 
-               } else if (premium.users.includes(message.author.id)) {
-                 message.channel.send('user has premium'); 
-               } else {
-                 message.channel.send('u need premium to run this command!!!');
-               }
-             }
+ if (premium.guilds.includes(message.guild.id)) {
+       if(!message.member.hasPermission('MANAGE_MESSAGES')){
+        return message.channel.send(':x: You need to have the manage messages permissions to create a backup in this server.');
+    }
+
+    backup.create(message.guild).then((backupData) => {
+
+        return message.channel.send('Backup created! Here is your ID: `'+backupData.id+'`! Use `'+config.prefix+'load-backup '+backupData.id+'` to load the backup on another server!');
+
+    }).catch(() => {
+
+        return message.channel.send(':x: An error occurred, please check if the bot is administrator!');
+
+    });
+ } else if (premium.users.includes(message.author.id)) {
+       if(!message.member.hasPermission('MANAGE_MESSAGES')){
+        return message.channel.send(':x: You need to have the manage messages permissions to create a backup in this server.');
+    }
+
+    backup.create(message.guild).then((backupData) => {
+
+        return message.channel.send('Backup created! Here is your ID: `'+backupData.id+'`! Use `'+config.prefix+'load-backup '+backupData.id+'` to load the backup on another server!');
+
+    }).catch(() => {
+
+        return message.channel.send(':x: An error occurred, please check if the bot is administrator!');
+
+    });
+ } else {
+   
+ }
+}
   
   
 
