6733a5a7a51852ca510efa4388ee2e32246c86cf
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-08-02 17:09:21 +0200

    Create backup.js

---
+++ commands/backup.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const premium = require('./../premium.json')
+
+
+
+
+module.exports.run = async (bot, message, args) => {
+               
+               if (premium.guilds.includes(message.guild.id)) {
+                 message.channel.send('guild has premium'); 
+               } else if (premium.users.includes(message.author.id)) {
+                 message.channel.send('user has premium'); 
+               } else {
+                 message.channel.send('u need premium to run this command!!!');
+               }
+             }
+  
+  
+
+module.exports.help = {
+  name:"backup"
+}
