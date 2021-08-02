e7a931c9a34866172a2ec49eb26d3a0f4bbc32c3
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-08-02 17:23:21 +0200

    Update backupinfo.js

--- commands/backupinfo.js
+++ commands/backupinfo.js
@@ -1,1 +1,87 @@
+const Discord = require("discord.js");
+const premium = require('./../premium.json')
+const backup = require('discord-backup');
 
+
+
+
+module.exports.run = async (bot, message, args) => {
+               
+ if (premium.guilds.includes(message.guild.id)) {
+    if(!message.member.hasPermission('MANAGE_MESSAGES')){
+        return message.channel.send(':x: You need to have the manage messages permissions to create a backup in this server.');
+    }
+
+    const backupID = args.join(' ');
+
+    if (!backupID)
+        return message.channel.send(':x: Please specify a valid backup ID!');
+
+    backup.fetch(backupID).then((backup) => {
+
+        const date = new Date(backup.data.createdTimestamp);
+        const yyyy = date.getFullYear().toString(), mm = (date.getMonth()+1).toString(), dd = date.getDate().toString();
+        const formattedDate = `${yyyy}/${(mm[1]?mm:"0"+mm[0])}/${(dd[1]?dd:"0"+dd[0])}`;
+
+        const embed = new Discord.MessageEmbed()
+            .setAuthor('ℹ️ Backup', backup.data.iconURL)
+            .addField('Server name', backup.data.name)
+            .addField('Size', backup.size + ' kb')
+            .addField('Created at', formattedDate)
+            .setFooter('Backup ID: '+backup.id);
+
+        return message.channel.send(embed);
+
+    }).catch((err) => {
+
+        if (err === 'No backup found')
+            return message.channel.send(':x: No backup found for ID '+backupID+'!');
+        else
+            return message.channel.send(':x: An error occurred: '+(typeof err === 'string') ? err : JSON.stringify(err));
+
+    });
+
+ } else if (premium.users.includes(message.author.id)) {
+    if(!message.member.hasPermission('MANAGE_MESSAGES')){
+        return message.channel.send(':x: You need to have the manage messages permissions to create a backup in this server.');
+    }
+
+    const backupID = args.join(' ');
+
+    if (!backupID)
+        return message.channel.send(':x: Please specify a valid backup ID!');
+
+    backup.fetch(backupID).then((backup) => {
+
+        const date = new Date(backup.data.createdTimestamp);
+        const yyyy = date.getFullYear().toString(), mm = (date.getMonth()+1).toString(), dd = date.getDate().toString();
+        const formattedDate = `${yyyy}/${(mm[1]?mm:"0"+mm[0])}/${(dd[1]?dd:"0"+dd[0])}`;
+
+        const embed = new Discord.MessageEmbed()
+            .setAuthor('ℹ️ Backup', backup.data.iconURL)
+            .addField('Server name', backup.data.name)
+            .addField('Size', backup.size + ' kb')
+            .addField('Created at', formattedDate)
+            .setFooter('Backup ID: '+backup.id);
+
+        return message.channel.send(embed);
+
+    }).catch((err) => {
+
+        if (err === 'No backup found')
+            return message.channel.send(':x: No backup found for ID '+backupID+'!');
+        else
+            return message.channel.send(':x: An error occurred: '+(typeof err === 'string') ? err : JSON.stringify(err));
+
+    });
+
+ } else {
+   message.channel.send('u need premium to run this command!!!');
+ }
+}
+  
+  
+
+module.exports.help = {
+  name:"backupinfo"
+}
